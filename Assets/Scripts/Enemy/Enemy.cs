f42b42dcaa972164e90983c0fd6ac127406a9655
wankcn <wankcn@icloud.com>
2020-10-30 22:31:57 +0800

    add-enemy-function

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    // 移动的方法
+    public void MoveToTarget()
+    {
+        FilpDirection();
+    }
+
+    // 攻击玩家
+    public void AttackAction()
+    {
+    }
+
+    // 对炸弹进行技能攻击
+    public void SkillAction()
+    {
+    }
+
+    // 巡逻过程中需要左右翻转 他一定是在移动的过程中进行调用的
+    public void FilpDirection()
+    {
     }
 }
