e2a8e4fd715875a9210e869c7070df603dce26a9
wankcn <wankcn@icloud.com>
2020-11-02 20:09:06 +0800

    修改Enemy脚本

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,20 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Movement")] public float speed; // 移动速度
+    public Transform ponitA, pointB;
+    public Transform targetPonit; // 目标点
+
+    public List<Transform> attackList = new List<Transform>();
+
     void Start()
     {
+        targetPonit = ponitA;
     }
 
     void Update()
     {
+        MoveToTarget();
     }
-
+    
     // 移动的方法
     public void MoveToTarget()
     {
+        // 朝着目标进行移动（当前坐标，目标坐标，距离的Delta值,确保在不同的机型得到同样的效果）
+        transform.position = Vector2.MoveTowards(transform.position, targetPonit.position,
+            speed * Time.deltaTime);
         FilpDirection();
     }
 
