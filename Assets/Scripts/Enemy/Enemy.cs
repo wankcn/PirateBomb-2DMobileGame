d1cbc19fda4f8dc644670be178f9bbf768705227
wankcn <wankcn@icloud.com>
2020-11-03 12:34:11 +0800

    修改敌人脚本，增加攻击列表内容

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -13,15 +13,18 @@ public class Enemy : MonoBehaviour
 
     void Start()
     {
-        targetPonit = ponitA;
+        SwitchPoint();
     }
 
     void Update()
     {
+        // 无限趋近于目标点
+        if (Mathf.Abs(transform.position.x - targetPonit.position.x) < 0.01f)
+            SwitchPoint();
         MoveToTarget();
     }
-    
-    // 移动的方法
+
+    /// 移动的方法
     public void MoveToTarget()
     {
         // 朝着目标进行移动（当前坐标，目标坐标，距离的Delta值,确保在不同的机型得到同样的效果）
@@ -30,18 +33,50 @@ public class Enemy : MonoBehaviour
         FilpDirection();
     }
 
-    // 攻击玩家
+    /// 攻击玩家
     public void AttackAction()
     {
     }
 
-    // 对炸弹进行技能攻击
+
+    /// 对炸弹进行技能攻击
     public void SkillAction()
     {
     }
 
-    // 巡逻过程中需要左右翻转 他一定是在移动的过程中进行调用的
+    /// 巡逻过程中需要左右翻转 他一定是在移动的过程中进行调用的
     public void FilpDirection()
     {
+        // 在目标点的左侧，向右追不需要翻转
+        if (transform.position.x < targetPonit.position.x)
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        else
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+    }
+
+
+    /// 判断当前坐标与PointA和B的位置，将移动的目标点设置为距离远的那一个
+    public void SwitchPoint()
+    {
+        // A点于当前怪物的距离>B点到当前怪物的距离说明A点远
+        if (Mathf.Abs(ponitA.position.x - transform.position.x)
+            > Mathf.Abs(pointB.position.x - transform.position.x))
+            targetPonit = ponitA;
+        else
+            targetPonit = pointB;
+    }
+
+    /// 物体进入检测范围就添加进攻击列表里
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        // 如果攻击列表不包含，再添加进列表里 添加的是transform
+        if (!attackList.Contains(other.transform))
+            attackList.Add(other.transform);
+    }
+
+    /// 物体走出检测范围从攻击列表里移除
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        attackList.Remove(other.transform);
     }
 }
