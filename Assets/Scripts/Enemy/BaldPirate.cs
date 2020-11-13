760131a78147601a0ef1defab94cd485f083065a
wankcn <wankcn@icloud.com>
2020-11-13 16:51:03 +0800

    增加了BigGuy的预制体

---
+++ Assets/Scripts/Enemy/BaldPirate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaldPirate : Enemy, IDamageable
+{
+    /// 事件方法执行在吹灭动画里
+    public void SetOff()
+    {
+        targetPonit.GetComponent<Bomb>().TurnOff();
+    }
+
+    public void GetHit(float damage)
+    {
+        enemyHP -= damage;
+        if (enemyHP < 1)
+        {
+            enemyHP = 0;
+            isDead = true;
+        }
+
+        anim.SetTrigger("hit");
+    }
+}
